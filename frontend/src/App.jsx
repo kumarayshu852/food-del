81669201fad1f43a530a4dba23952a19ddbabf97
kumarayshu852 <kumarayshu852@gmail.com>
2026-03-14 18:03:33 +0530

    fix case sensitive path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
-import PlaceOrder from "./pages/placeOrder/PlaceOrder";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import Footer from "./components/Footer/Footer";
