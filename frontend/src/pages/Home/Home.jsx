5441a219db1b08a77dbf04f7dde4a2c39e9f792e
kumarayshu852 <kumarayshu852@gmail.com>
2026-03-14 18:21:04 +0530

    fix issue

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css'
 import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import { useState } from 'react';
-import FoodDisplay from "../../components/FoodDisplay/foodDisplay";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 import FoodItem from "../../components/FoodItem/FoodItem";
 import AppDownload from "../../components/AppDownload/AppDownload";
 
